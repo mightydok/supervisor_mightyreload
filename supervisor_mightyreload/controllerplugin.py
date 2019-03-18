ca59228440d6e0d8c83ba92f550f001d990043aa
Vitaliy Okulov <v.okulov@city-mobil.ru>
2019-03-18 18:54:32 +0300

    Remove unused functions

--- supervisor_mightyreload/controllerplugin.py
+++ supervisor_mightyreload/controllerplugin.py
@@ -16,65 +16,9 @@ class MightyReloadControllerPlugin(ControllerPluginBase):
         self.ctl = controller
         self.supervisor = controller.get_server_proxy('supervisor')
         self.mightyreload = controller.get_server_proxy('mightyreload')
-        self.match_group = bool(int(config.get('match_group', '1')))
-
-    # Quick restart and status block on thread
-    def _match_process(self, process, pattern):
-        name = process['name']
-        if self.match_group:
-            name = "%s:%s" % (process['group'], process['name'])
-        return fnmatch.fnmatch(name, pattern)
-
-    def _expand_wildcards(self, arg, command):
-        patterns = arg.split()
-        supervisor = self.ctl.get_supervisor()
-        if 'all' in patterns:
-            # match any process name
-            patterns = ['*']
-
-        # Create list for threading restart
-        threads = []
-        # Create set for monitoring
-        procgroupset = set()
-        for process in supervisor.getAllProcessInfo():
-            for pattern in patterns:
-                if self._match_process(process, pattern):
-                    # Fill set with all
-                    procgroupset.add(process['group'])
-                    t = Thread(target=self.ctl.onecmd, args=('%s %s:%s' % (command, process['group'],
-                                                                           process['name']), ))
-                    t.start()
-                    threads.append(t)
-
-        for t in threads:
-            t.join()
-
-        if not threads:
-            self.ctl.output('No process matched given expression.')
-
-    def _wrap_help(self, command):
-        self.ctl.output('The same as %s, but accepts wildcard expressions to match the process name.' % command)
-        self.ctl.output('m%s a* - %ss all processes begining with "a".' % (command, command))
-
-    def do_mstop(self, arg):
-        self._expand_wildcards(arg, command='stop')
-    def do_mstart(self, arg):
-        self._expand_wildcards(arg, command='start')
-    def do_mrestart(self, arg):
-        self._expand_wildcards(arg, command='restart')
-    def do_mstatus(self, arg):
-        self._expand_wildcards(arg, command='status')
-    def help_mstop(self):
-        return self._wrap_help('stop')
-    def help_mstart(self):
-        return self._wrap_help('start')
-    def help_mrestart(self):
-        return self._wrap_help('restart')
-    def help_mstatus(self):
-        return self._wrap_help('status')
 
     # Graceful update of configuration
-    def do_mightyreload(self, arg):
+    def do_mreload(self, arg):
         def log(name, message):
             self.ctl.output("%s: %s" % (name, message))
 
@@ -151,8 +95,8 @@ class MightyReloadControllerPlugin(ControllerPluginBase):
             supervisor.addProcessGroup(gname)
             log(gname, "added process group")
 
-    def help_mightyreload(self):
-        self.ctl.output("mightyreload\t\tGracefull update of supervisord configuration")
+    def help_mreload(self):
+        self.ctl.output("mreload\t\tGracefull update of supervisord configuration")
 
 
 def make_mightyreload_controllerplugin(controller, **config):
